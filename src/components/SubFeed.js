759a5c3baec3eaea69a352541d0c2f10c83dae4c
Kota Ueshima <ueshima@usc.edu>
2023-02-05 01:06:41 -0800

    update

--- src/components/SubFeed.js
+++ src/components/SubFeed.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import SubPosts from "./SubPosts";
 import URL from "./url";
 
 function SubFeed({ show }) {
@@ -47,23 +48,12 @@ function SubFeed({ show }) {
       <div className="h-[60vh] w-full flex flex-col overflow-y-scroll">
         {subPosts.map(({ id, username, content, date }) => {
           return (
-            <div
+            <SubPosts
               key={id}
-              className="my-3 w-[90%] mx-auto p-4 bg-primary rounded-md flex flex-col space-y-2"
-            >
-              <div className="flex flex-row space-x-2 items-center justify-end">
-                <h3 className="text-xs text-white">{username}</h3>
-                <img
-                  className="w-6 h-6 rounded-full"
-                  src="https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg"
-                  alt="Rounded avatar"
-                />
-              </div>
-              <div className="flex flex-col items-end">
-                <p className="text-white">{content}</p>
-                <p className="mt-2 text-secondary text-xs">{date}</p>
-              </div>
-            </div>
+              username={username}
+              content={content}
+              date={date}
+            />
           );
         })}
       </div>
