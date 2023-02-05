2282f3228f8cfc2d020cdaa2d391671616662205
Kota Ueshima <ueshima@usc.edu>
2023-02-05 00:49:06 -0800

    a

--- src/components/SubFeed.js
+++ src/components/SubFeed.js
@@ -7,7 +7,7 @@ function SubFeed({ show }) {
   const [subPosts, setSubPosts] = useState([]);
 
   useEffect(() => {
-    fetch("/get_post", {
+    fetch("/get_sub_post", {
       method: "POST",
       headers: {
         Accepts: "application/json",
@@ -17,34 +17,9 @@ function SubFeed({ show }) {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setSubPosts(data);
       });
   }, []);
-
-  const example = [
-    {
-      id: 1,
-    },
-    {
-      id: 2,
-    },
-    {
-      id: 3,
-    },
-    {
-      id: 4,
-    },
-    {
-      id: 5,
-    },
-    {
-      id: 6,
-    },
-    {
-      id: 7,
-    },
-  ];
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -70,14 +45,14 @@ function SubFeed({ show }) {
       } ease-in-out duration-1000 mt-10 absolute right-[2vw] h-[80vh] w-[23vw] bg-white rounded-md`}
     >
       <div className="h-[60vh] w-full flex flex-col overflow-y-scroll">
-        {example.map(({ id }) => {
+        {subPosts.map(({ id, username, content, date }) => {
           return (
             <div
               key={id}
               className="my-3 w-[90%] mx-auto p-4 bg-primary rounded-md flex flex-col space-y-2"
             >
               <div className="flex flex-row space-x-2 items-center justify-end">
-                <h3 className="text-xs text-white">Samantha Gray</h3>
+                <h3 className="text-xs text-white">{username}</h3>
                 <img
                   className="w-6 h-6 rounded-full"
                   src="https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg"
@@ -85,8 +60,8 @@ function SubFeed({ show }) {
                 />
               </div>
               <div className="flex flex-col items-end">
-                <p className="text-white">Wow awesome, that really sick!</p>
-                <p className="mt-2 text-secondary text-xs">8:23 AM</p>
+                <p className="text-white">{content}</p>
+                <p className="mt-2 text-secondary text-xs">{date}</p>
               </div>
             </div>
           );
